2c196572233a6704e695d3dd0a3f8e0ad006d570
doaaxv <doaa.mh.uni@gmail.com>
2019-11-29 18:38:49 +0300

    deployment fixes

---
+++ build/precache-manifest.6fc783065e4baadc24db1cf44898dd18.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b30454651531d0f0bd8447bc566de21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d65b086d38925e0b8d8",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5abd957617a46e497c10",
+    "url": "/static/css/main.017803cc.chunk.css"
+  },
+  {
+    "revision": "9d65b086d38925e0b8d8",
+    "url": "/static/js/2.7377195b.chunk.js"
+  },
+  {
+    "revision": "5abd957617a46e497c10",
+    "url": "/static/js/main.98e9bd81.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "71c806428f9d8c76f8dd491ee177382c",
+    "url": "/static/media/marker.71c80642.png"
+  },
+  {
+    "revision": "093ca05c98d60470a081cca65094b08f",
+    "url": "/static/media/shalihat-logo.093ca05c.png"
+  },
+  {
+    "revision": "7dfe07aee3fca4d3f39c4fe0aeb35a8f",
+    "url": "/static/media/villa-pic.7dfe07ae.jpg"
+  },
+  {
+    "revision": "359ab2dbecdf9e08853e2500eb98b840",
+    "url": "/static/media/villa2-pic.359ab2db.jpg"
+  },
+  {
+    "revision": "4af037bb5bc39a10db9bfeed0b93f2bb",
+    "url": "/static/media/villaImg.4af037bb.jpg"
+  }
+]);
